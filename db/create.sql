6bace5752551422140dccc9648181638217fad3e
Tobias Hoogwerf <113464@emmauscollege.nl>
2025-02-21 10:42:05 +0000

    hoodies

--- db/create.sql
+++ db/create.sql
@@ -14,7 +14,7 @@ CREATE TABLE catogories (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
   name VARCHAR(255),
-  description TEXT,
+  description HOODIES,
   price NUMERIC(10, 2)
 );
 
@@ -28,9 +28,9 @@ CREATE TABLE catogories (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Hoodie', '', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('T-shirt', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('rugtas', 'The Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
+insert into products (name, description, code, price) values ('Hoodie rood', 'Rode royalistiq hoodie, groot logo in het midden.', '816905633-0', 10.5);
+insert into products (name, description, code, price) values ('Hoodie blauw', 'Blauwe royalistiq hoodie, groot logo in het midden.', '077030122-3', 11);
+insert into products (name, description, code, price) values ('Hoodie zwart', 'zwarte royalistiq hoodie, groot logo in het midden.', '445924201-X', 13.5);
 insert into products (name, description, code, price) values ('sokken', 'el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('bandana', 'A (Almost a Bride) Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('zonnebril', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
