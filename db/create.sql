906bdacbaf77fe3b5bacf31ef5560c3e008db9bd
Tobias Hoogwerf <113464@emmauscollege.nl>
2025-02-23 16:24:08 +0000

    n op m relatie en verzendkosten

--- db/create.sql
+++ db/create.sql
@@ -9,31 +9,37 @@ CREATE TABLE products (
   description TEXT,
   price NUMERIC(10, 2),
   voorraad NUMERIC(10),
+  verzendkosten NUMERIC(10),
   catogories_id INTEGER,
-  beoordeling_id INTEGER
+  beoordeling_id INTEGER,
+  products_id INTEGER,
+  seizoenen_id INTEGER
 );
 
 CREATE TABLE catogories (
+  catogories_id INTEGER,
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
 );
 
 CREATE TABLE seizoenen (
+  seizoenen_id INTEGER,
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
-);
-
-CREATE TABLE products_seizoenen (
-   PRIMARY KEY (products_id, seizoenen_id), 
-   FOREIGN KEY (seizoenen_id) REFERENCES products(products_id),
-   FOREIGN KEY (products_id) REFERENCES seizoenen(seizoenen_id),
-   products_id INTEGER,   
-   seizoenen_id INTEGER
 );
 
 CREATE TABLE beoordeling (
+  beoordeling_id INTEGER,
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
+);
+
+CREATE TABLE geschiktheid (
+  products_id INTEGER,   
+  seizoenen_id INTEGER,
+  PRIMARY KEY (products_id, seizoenen_id), 
+  FOREIGN KEY (products_id) REFERENCES products(products_id),
+  FOREIGN KEY (seizoenen_id) REFERENCES seizoenen(seizoenen_id)
 );
 
 
@@ -46,34 +52,61 @@ CREATE TABLE beoordeling (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Hoodie rood', 'Rode royalistiq hoodie, groot logo in het midden.', '816905633-0', 21, 20, 1, 1);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Hoodie blauw', 'Blauwe royalistiq hoodie, groot logo in het midden.', '077030122-3', 21, 26, 1, 2);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Hoodie zwart', 'zwarte royalistiq hoodie, groot logo in het midden.', '445924201-X', 21, 8, 1, 2);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('T-shirt rood', 'rood royalistiq t-shirt, goede kwaliteit, logo in het midden.', '693155505-7', 14, 30, 2, 1);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('T-shirt blauw', 'blauw royalistiq t-shirt, goede kwaliteit, logo in het midden.', '686928463-6', 14, 22, 2, 1);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('T-shirt zwart', 'zwart royalistiq t-shirt, goede kwaliteit, logo in het midden.', '492662523-7', 14, 32, 2, 1);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Pet rood', 'rode royalistiq pet, zit comfortabel, logo in het midden.', '003832762-4', 9, 8, 3, 1);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Pet blauw', 'blauwe royalistiq pet, zit comfortabel, logo in het midden.', '578834325-2', 9, 30, 3, 3);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Pet zwart', 'zwarte royalistiq pet, zit comfortabel, logo in het midden.', '5674821169-5', 9, 21, 3, 2);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Het grote royalistiq boek', 'het al zo grote royalistiq boek over de youtube carrière van royalistiq .', '882374623-1', 19.99, 27, 4, 2);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Etui', 'royalistiq etui, veel ruimte, handig voor op school.', '849275978-6', 8.50, 42, 4, 4);
-insert into products (name, description, code, price, voorraad, catogories_id, beoordeling_id) values ('Exlusive bandana', 'de legendarische OG royalistiq bandana, gedragen door royalistiq in het royalistiq live-event', '654890345-9', 50, 3, 4, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (1, 'Hoodie rood', 'Rode royalistiq hoodie, groot logo in het midden.', '816905633-0', 21, 20, 3, 1, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (2, 'Hoodie blauw', 'Blauwe royalistiq hoodie, groot logo in het midden.', '077030122-3', 21, 26, 3, 1, 2);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (3, 'Hoodie zwart', 'zwarte royalistiq hoodie, groot logo in het midden.', '445924201-X', 21, 8, 3, 1, 2);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (4, 'T-shirt rood', 'rood royalistiq t-shirt, goede kwaliteit, logo in het midden.', '693155505-7', 14, 30, 3, 2, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (5, 'T-shirt blauw', 'blauw royalistiq t-shirt, goede kwaliteit, logo in het midden.', '686928463-6', 14, 22, 3, 2, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (6, 'T-shirt zwart', 'zwart royalistiq t-shirt, goede kwaliteit, logo in het midden.', '492662523-7', 14, 32, 3, 2, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (7, 'Pet rood', 'rode royalistiq pet, zit comfortabel, logo in het midden.', '003832762-4', 9, 8, 3, 3, 1);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (8, 'Pet blauw', 'blauwe royalistiq pet, zit comfortabel, logo in het midden.', '578834325-2', 9, 30, 3, 3, 3);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (9, 'Pet zwart', 'zwarte royalistiq pet, zit comfortabel, logo in het midden.', '5674821169-5', 9, 21, 3, 3, 2);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (10 ,'Het grote royalistiq boek', 'het al zo grote royalistiq boek over de youtube carrière van royalistiq .', '882374623-1', 19.99, 27, 3, 4, 2);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (11, 'Etui', 'royalistiq etui, veel ruimte, handig voor op school.', '849275978-6', 8.50, 42, 3, 4, 4);
+insert into products (products_id, name, description, code, price, voorraad, verzendkosten, catogories_id, beoordeling_id) values (12, 'Exlusive bandana', 'de legendarische OG royalistiq bandana, gedragen door royalistiq in het royalistiq live-event', '654890345-9', 50, 3, 3, 4, 1);
 
-insert into catogories (name) values ('Hoodies');
-insert into catogories (name) values ('T-shirts');
-insert into catogories (name) values ('petten');
-insert into catogories (name) values ('accessoires');
+insert into catogories (catogories_id, name) values (1, 'Hoodies');
+insert into catogories (catogories_id, name) values (2, 'T-shirts');
+insert into catogories (catogories_id, name) values (3, 'petten');
+insert into catogories (catogories_id, name) values (4, 'accessoires');
 
-insert into seizoenen (name) values ('zomer');
-insert into seizoenen (name) values ('herfst');
-insert into seizoenen (name) values ('winter');
-insert into seizoenen (name) values ('lente');
+insert into beoordeling (beoordeling_id, name) values (1, 'geweldig');
+insert into beoordeling (beoordeling_id, name) values (2, 'prima');
+insert into beoordeling (beoordeling_id, name) values (3, 'matig');
+insert into beoordeling (beoordeling_id, name) values (4, 'slecht');
 
-insert into products_seizoenen (products_id, seizoenen_id) values ('prima');
-insert into products_seizoenen (products_id, seizoenen_id) values ('matig');
-insert into products_seizoenen (products_id, seizoenen_id) values ('slecht');
+insert into seizoenen (seizoenen_id, name) values (1, 'zomer');
+insert into seizoenen (seizoenen_id, name) values (2, 'herfst');
+insert into seizoenen (seizoenen_id, name) values (3, 'winter');
+insert into seizoenen (seizoenen_id, name) values (4, 'lente');
 
-insert into beoordeling (name) values ('geweldig');
-insert into beoordeling (name) values ('prima');
-insert into beoordeling (name) values ('matig');
-insert into beoordeling (name) values ('slecht');
+insert into geschiktheid (products_id, seizoenen_id) values (1,2);
+insert into geschiktheid (products_id, seizoenen_id) values (1,3);
+insert into geschiktheid (products_id, seizoenen_id) values (2,2);
+insert into geschiktheid (products_id, seizoenen_id) values (2,3);
+insert into geschiktheid (products_id, seizoenen_id) values (3,2);
+insert into geschiktheid (products_id, seizoenen_id) values (3,3);
+insert into geschiktheid (products_id, seizoenen_id) values (4,1);
+insert into geschiktheid (products_id, seizoenen_id) values (4,4);
+insert into geschiktheid (products_id, seizoenen_id) values (5,1);
+insert into geschiktheid (products_id, seizoenen_id) values (5,4);
+insert into geschiktheid (products_id, seizoenen_id) values (6,1);
+insert into geschiktheid (products_id, seizoenen_id) values (6,4);
+insert into geschiktheid (products_id, seizoenen_id) values (7,1);
+insert into geschiktheid (products_id, seizoenen_id) values (7,4);
+insert into geschiktheid (products_id, seizoenen_id) values (8,1);
+insert into geschiktheid (products_id, seizoenen_id) values (8,4);
+insert into geschiktheid (products_id, seizoenen_id) values (9,1);
+insert into geschiktheid (products_id, seizoenen_id) values (9,4);
+insert into geschiktheid (products_id, seizoenen_id) values (10,1);
+insert into geschiktheid (products_id, seizoenen_id) values (10,2);
+insert into geschiktheid (products_id, seizoenen_id) values (10,3);
+insert into geschiktheid (products_id, seizoenen_id) values (10,4);
+insert into geschiktheid (products_id, seizoenen_id) values (11,1);
+insert into geschiktheid (products_id, seizoenen_id) values (11,2);
+insert into geschiktheid (products_id, seizoenen_id) values (11,3);
+insert into geschiktheid (products_id, seizoenen_id) values (11,4);
+insert into geschiktheid (products_id, seizoenen_id) values (12,1);
+insert into geschiktheid (products_id, seizoenen_id) values (12,2);
+insert into geschiktheid (products_id, seizoenen_id) values (12,3);
+insert into geschiktheid (products_id, seizoenen_id) values (12,4);
